da616aa946ca2a69d5632ee0f2f191d180bce1a8
ferromauro <ferromauro@yahoo.it>
2021-01-29 22:00:41 +0000

    Script 01

---
+++ script_01.c
@@ -0,0 +1,13 @@
+/* Primo script da compilare via linea di comando.
+ * Su Linux utilizzare il comando gcc per la compilazione del programma.
+ * 
+ * ESEMPIO: gcc script_01.c -o nome_file_eseguibile 
+ * (Nella cartella 'compiled' avete tutti gli script compilati)
+ *  */
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World! This is a native C program compiled on the command line.\n");
+	return 0;
+}
