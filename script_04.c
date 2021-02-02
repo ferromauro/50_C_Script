629497f3f6b131e84bd5cb2adcb311bba479f557
StartG17 <ferromauro@yahoo.it>
2021-02-02 14:39:22 +0100

    Script 05

---
+++ script_04.c
@@ -0,0 +1,12 @@
+// Progamma per stampare i numeri da n0 a 10 attraverso l'utilizzo di un ciclo for.
+#include <stdio.h>
+#define NUMERO 10
+int main()
+{n
+	int i;i
+	for(i=0; i <=NUMERO; i++)n
+		printf("%d \n",i);\
+	printf("Programma completato\n");
+	return(0);
+}
+	     	
